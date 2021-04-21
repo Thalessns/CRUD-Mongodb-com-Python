ba48e7533afd3cb03acf10ee4761f3ab61f77a30
Thalessns <63682245+Thalessns@users.noreply.github.com>
2021-04-21 14:01:56 -0300

    v1.1

--- uteis/__init__.py
+++ uteis/__init__.py
@@ -2,10 +2,10 @@
 def msg(txt):
     return f'{txt:^100}';
 
-# Função espacamento
+# Função Espacamento
 def espaco(var):
     return f'.' * (30 - len(str(var.strip())));
 
-# Texto aleatorio
+# Texto Aleatorio
 def texto(txt):
     return f'Você não pode {txt} este registro!';
